b84b557e67c82a43a65f77a611d94341e9053e14
kacper <kacper.fonder@gmail.com>
2018-05-28 23:31:41 +0200

    adding api data to select

--- js/app.js
+++ js/app.js
@@ -1,21 +1,43 @@
-let urlApi = 'https://api.fixer.io/latest'
+$(function(){
 
-  
-$.ajax({
-    url: urlApi,
-    method: "GET",
-    dataType: "json"
-  })
+let urlApi = 'https://api.fixer.io/latest?base=AUD';
+let from = document.querySelector('.from');
+let to = document.querySelector('.to');
+
+function loadCurrencies() {
+
+    $.ajax({
+        url: urlApi,
+        method: "GET",
+        dataType: "json"
+    })
     .done(function(response) {
-      console.log(response);
-    });
+        console.log(response); 
+        let rates = response.rates;
+        let currencies = '';
+        for(let key in rates) {
+          
+            currencies += '<option val=' + key + '>' + key + '</option>'
+        }
+        from.innerHTML = currencies
+        to.innerHTML = currencies
+    });  
+};
+    $('.from').change(function(){
+        let value = $('from').val();
+        
+        
+        $.get('https://api.fixer.io/latest?base='+value, function(newRates){
+            rates = newRates.rates
+        })
+    })
 
 
-// $.ajax({
-//     url: urlApi, 
-//     type: "GET",
-//     success: function(result){
-//         $('img').attr('src',result.icon_url);
-//         $('.jokeContent').text(result.value)
-// }});  
+loadCurrencies();
+});
+    
+     
+
+
+
 
